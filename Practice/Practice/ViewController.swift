46b3b72f1abd1325fc16693e80ab21709debbe80
shen <shen@shendeMacBook-Air.local>
2017-02-14 14:49:42 +0800

    practice table view with sections

--- Practice/Practice/ViewController.swift
+++ Practice/Practice/ViewController.swift
@@ -2,24 +2,38 @@
 //  ViewController.swift
 //  Practice
 //
-//  Created by asasdasasd on 2017/2/9.
+//  Created by shen on 2017/2/14.
 //  Copyright © 2017年 asasdasasd. All rights reserved.
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
+    var disposebag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.automaticallyAdjustsScrollViewInsets = false
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
