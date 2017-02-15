ef04826bf06704d3a2c499988a221b683efd5865
shen <shen@shendeMacBook-Air.local>
2017-02-15 15:54:02 +0800

    partial updates table view setup

---
+++ Practice/Practice/NumberSectionView.swift
@@ -0,0 +1,14 @@
+//
+//  NumberSectionView.swift
+//  Practice
+//
+//  Created by asasdasasd on 2017/2/15.
+//  Copyright © 2017年 asasdasasd. All rights reserved.
+//
+
+import UIKit
+
+class NumberSectionView: UICollectionReusableView {
+        
+    @IBOutlet weak var value: UILabel!
+}
