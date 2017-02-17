f9779022caebd43fa5b05a0b546d8cd5003c37f0
shen <shen@shendeMacBook-Air.local>
2017-02-17 13:49:25 +0800

    table view with editing commands view set

---
+++ Practice/Practice/TableViewWithEditingCommandsController.swift
@@ -0,0 +1,35 @@
+//
+//  TableViewWithEditingCommandsController.swift
+//  Practice
+//
+//  Created by shen on 2017/2/17.
+//  Copyright © 2017年 asasdasasd. All rights reserved.
+//
+
+import UIKit
+
+class TableViewWithEditingCommandsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
